4e187f9dc088049c7ae84748004ccf5ebfd02b68
Terrilhhl461aur0ynMooreozd2fpnql <lyubomiraz243816758@rambler.ru>
2025-12-04 08:27:50 +0800

    add spec.sass, registry.adoc, endpoint.ts

---
+++ frontend/web/src/configuration-provider-frontend/endpoint.ts
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+export const spec-handler = () => { return null; };
---
+++ subscriptions-dist.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\subscriptions-dist.go
+// Generated at: 2025-10-03T10:36:41.448Z
